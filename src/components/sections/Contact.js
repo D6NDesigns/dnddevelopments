9df95826a2d8b48f288a392beb8bcd3f0d6e3a0b
James Dixon <jad_dixon@hotmail.com>
2019-08-18 20:22:33 +0100

    removed the example form code and ready to test following traversymedia tutorial

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import { navigate } from 'gatsby-link';
+// import { navigate } from 'gatsby-link';
 import PropTypes from 'prop-types';
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+// function encode(data) {
+//   return Object.keys(data)
+//     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+//     .join('&')
+// }
 
 export default class Contact extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
       isValidated: false,
-      isSubmitted: false,
     }
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error))
   }
 
   render() {
@@ -49,23 +33,12 @@ export default class Contact extends React.Component {
         <div className="col-md-6">
           <form
             name="dnd-contact-form"
-            method="post"
-            action="/contact/thanks/"
+            method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            onSubmit={this.handleSubmit}
           >
             {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
             <input type="hidden" name="form-name" value="dnd-contact-form" />
-            <div hidden>
-              <label>
-                Don’t fill this out:{' '}
-                <input 
-                  name="bot-field" 
-                  onChange={this.handleChange} 
-                />
-              </label>
-            </div>
             <div className="form-group">
               <label className="label" htmlFor={'name'}>
                 Your name
@@ -74,7 +47,6 @@ export default class Contact extends React.Component {
                 className="form-control"
                 type={'text'}
                 name={'name'}
-                onChange={this.handleChange}
                 id={'name'}
                 required={true}
               />
@@ -87,7 +59,6 @@ export default class Contact extends React.Component {
                 className="form-control"
                 type={'phone'}
                 name={'phone'}
-                onChange={this.handleChange}
                 id={'phone'}
                 required={true}
               />
@@ -100,7 +71,6 @@ export default class Contact extends React.Component {
                 className="form-control"
                 type={'email'}
                 name={'email'}
-                onChange={this.handleChange}
                 id={'email'}
                 required={true}
               />
@@ -112,10 +82,12 @@ export default class Contact extends React.Component {
               <textarea
                 className="form-control"
                 name={'message'}
-                onChange={this.handleChange}
                 id={'message'}
                 required={true}
               />
+            </div>
+            <div className="form-group">
+              <div data-netlify-recaptcha="true" />
             </div>
             <button className="btn btn-primary text-uppercase" type="submit">
               Send Message
