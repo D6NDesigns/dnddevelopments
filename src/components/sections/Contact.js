df268bfc6c9c8fd5f43998850f885d7b9645212c
James Dixon <jad_dixon@hotmail.com>
2019-08-18 20:09:20 +0100

    Last attempt before sacking it off for today

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -48,15 +48,15 @@ export default class Contact extends React.Component {
         </div>
         <div className="col-md-6">
           <form
-            name="contact"
+            name="dnd-contact-form"
             method="post"
-            action="POST"
+            action="/contact/thanks/"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
             onSubmit={this.handleSubmit}
           >
             {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="dnd-contact-form" />
             <div hidden>
               <label>
                 Don’t fill this out:{' '}
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -41,7 +41,7 @@ export default class Index extends React.Component {
             <div className="content">
               <h1>Contact</h1>
               <form
-                name="contactExample"
+                name="contact"
                 method="post"
                 action="/contact/thanks/"
                 data-netlify="true"
@@ -49,7 +49,7 @@ export default class Index extends React.Component {
                 onSubmit={this.handleSubmit}
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contactExample" />
+                <input type="hidden" name="form-name" value="contact" />
                 <div hidden>
                   <label>
                     Don’t fill this out:{' '}
