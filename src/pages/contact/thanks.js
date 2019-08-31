c17d73a0f0a4361bdc3773fb89f13d37ff4dbea5
James Dixon <jad_dixon@hotmail.com>
2019-08-31 10:49:48 +0100

    finished thankyou page

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,9 +7,12 @@ export default () => (
   <Layout>
     <section id="thanks" className="dnd-thanks dnd-branding dnd-section">
       <div className="container text-center">
+        <Link to="/" className="dnd-thanks-logo text-hide">
+          Back to homepage  
+        </Link>
         <h2 className="section-heading text-uppercase">Thank you</h2>
-        <h3 className="section-subheading text-muted">Thank you for you Contacting D&amp;D Developments.</h3>
-        <p className="text-muted">Your message has been recieved and we will contact you shortly via the details you provided.</p>
+        <h3 className="section-subheading text-muted">Thank you for you contacting D&amp;D Developments.</h3>
+        <p className="text-muted">Your message has been recieved and D&amp;D Developments will contact you shortly via the details you provided.</p>
         <Link 
           to="/"
           className="btn btn-primary text-uppercase"
