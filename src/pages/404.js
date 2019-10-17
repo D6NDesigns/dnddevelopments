69a4b2ed1537c190fdab44a11f25ee7a8143c945
jdixon <JDixon@webroot.com>
2019-10-17 16:20:16 +0100

    404 page restyle

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section id="not-found" className="dnd-thanks dnd-branding dnd-section">
+      <div className="container text-center">
+        <Link to="/" className="dnd-thanks-logo text-hide">
+          Back to homepage  
+        </Link>
+        <h2 className="section-heading text-uppercase">Oops!</h2>
+        <h3 className="section-subheading text-muted">404 - PAGE NOT FOUND.</h3>
+        <p className="text-muted">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+        <Link 
+          to="/"
+          className="btn btn-primary text-uppercase"
+        >
+          Back to homepage
+        </Link>
+      </div>
+    </section>
   </Layout>
 )
 
