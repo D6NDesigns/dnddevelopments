b28a183ceed96e158949644930ddfc2500c9b0e6
James Dixon <jad_dixon@hotmail.com>
2022-11-29 20:02:05 +0000

    All files commit

---
+++ src/components/sections/Intro.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const Intro = () => {
+  const title = '';
+  const heading = '';
+  const subheading = '';
+  const image = '';
+  return (
+    <header
+        id="intro"
+        className="dnd-intro dnd-branding"
+        // style={{
+        //   backgroundImage: `url(${
+        //     !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+        //   })`
+        // }}
+      >
+        <div className="container">
+          <div className="intro-text">
+            <h1 className="intro-lead-in">
+              {title}
+            </h1>
+            <h3 className="intro-heading text-uppercase">
+              {heading}
+            </h3>
+            <Link 
+              className="btn btn-primary btn-xl text-uppercase"
+              duration={350}
+              spy={true}
+              smooth={true}
+              to="services"
+            >
+              {subheading}
+            </Link>
+          </div>
+        </div>
+      </header>
+  )
+};
+
+export default Intro;
