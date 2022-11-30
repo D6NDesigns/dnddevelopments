39061273dc70f418366c05cf72053092e9f6bd98
James Dixon <jad_dixon@hotmail.com>
2022-11-30 11:42:24 +0000

    root file updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,25 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `D and D Developments`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'D & D Developments',
+    description:
+      'Midlands Extensions, Renovations, New Builds and Landscaping',
+    theme: '#E8B90E',
+    pages: [
+      'Services',
+      'Portfolio',
+      'About',
+      'Contact'
+    ],
+    services: [
+      'Extensions',
+      'Renovations',
+      'New Builds',
+      'Landscaping'
+    ],
+    siteUrl: `https://dandddevelopments.uk/`,
   },
-  plugins: ["gatsby-plugin-image", {
+  plugins: ['gatsby-plugin-sass', "gatsby-plugin-image", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
